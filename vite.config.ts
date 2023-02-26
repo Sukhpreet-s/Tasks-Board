9490987f89f85ab75828bfe79fae19cbe42af6e9
sukhpreet-s <sukhpreets6718@gmail.com>
2023-02-26 21:16:37 +0000

    add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build'
-  },
+  base: "Tasks-Board",
   resolve: {
     alias: {
       "#root": __dirname,
