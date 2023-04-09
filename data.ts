a0118886cfbf422198e07265ad0a35239e9e97e2
sukhpreet-s <sukhpreets6718@gmail.com>
2023-04-09 04:09:14 +0000

    changed items displayed in list

--- data.ts
+++ data.ts
@@ -2,17 +2,20 @@ const data: Record<string, TaskList> = {
   "list-1": {
     title: "Tasks To Do",
     tasks: {
-      "task-1": { title: "Research about healthy diet" },
-      "task-2": { title: "Compelte an assessment to submit" },
-      "task-3": { title: "Walk the dog" },
+      "task-1": { title: "Allow ability to create new lists" },
+      "task-2": { title: "Save the data for each user" },
+      "task-3": { title: "Ability to store details of a task" },
     },
   },
   "list-2": {
     title: "Tasks In Progress",
     tasks: {
-      "task-4": { title: "Research about healthy diet" },
-      "task-5": { title: "Compelte an assessment to submit" },
-      "task-6": { title: "Walk the dog" },
+      "task-4": { title: "Refactoring components" },
+      "task-5": { title: "Ability to edit a task" },
+      "task-6": { title: "Ability to add a task" },
+      "task-7": {
+        title: "Able to drag tasks within a list or to other lists.",
+      },
     },
   },
 };
